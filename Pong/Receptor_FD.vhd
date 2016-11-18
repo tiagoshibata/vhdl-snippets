b86e8afc1b0b1cbe0c6d063443369777e5642cfb
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-11-18 10:52:32 -0200

    Sample receiver and use most common value

--- Pong/Receptor_FD.vhd
+++ Pong/Receptor_FD.vhd
@@ -12,7 +12,8 @@ entity Receptor_FD is port (
 
 architecture Receptor_FD_arch of Receptor_FD is
     signal Sdata: STD_LOGIC_VECTOR(10 downto 0);
-    signal Sparity, Ssample: STD_LOGIC;
+    signal one_count, zero_count: STD_LOGIC_VECTOR(18 downto 0) := (others => '0');
+    signal Sparity, Sserial_value: STD_LOGIC;
 
     component counter port (
         clk, reset, count: in std_logic;
@@ -32,12 +33,30 @@ architecture Receptor_FD_arch of Receptor_FD is
         parity: out STD_LOGIC
     ); end component;
 begin
-    data <= Sdata(8 downto 1);
-    Ssample <= tick;
-    sample <= Ssample;
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if busy_rx = '0' or tick = '1' then
+                zero_count <= (others => '0');
+                one_count <= (others => '0');
+            elsif serial = '1' then
+                one_count <= one_count + '1';
+            else
+                zero_count <= one_count + '1';
+            end if;
+            if zero_count > one_count then
+                Sserial_value <= '0';
+            else
+                Sserial_value <= '1';
+            end if;
+        end if;
+    end process;
+
+    data <= '0' & Sdata(7 downto 1);
+    sample <= tick;
     parity_ok <= Sparity xor Sdata(10);
 
     Iparity: parity port map (clk, Sdata(8 downto 1), Sparity);
     bit_counter: counter port map (clk, not busy_rx, tick, rx_bit_count);
-    Ishifter: shifter port map (clk, tick and busy_rx, serial, '0', open, (others => '0'), Sdata);
+    Ishifter: shifter port map (clk, tick and busy_rx, Sserial_value, '0', open, (others => '0'), Sdata);
 end Receptor_FD_arch;
